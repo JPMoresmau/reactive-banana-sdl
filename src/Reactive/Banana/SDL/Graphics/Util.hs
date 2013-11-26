bf885b0c04c49b79cdaf5e630451776904218844
jpmoresmau <jp@moresmau.fr>
2013-11-26 17:53:47 +0100

    Hlint suggestions

--- src/Reactive/Banana/SDL/Graphics/Util.hs
+++ src/Reactive/Banana/SDL/Graphics/Util.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeSynonymInstances, TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeSynonymInstances #-}
 module Reactive.Banana.SDL.Graphics.Util where
 
 import Reactive.Banana.SDL.Graphics.Types
@@ -10,6 +10,7 @@ import Graphics.UI.SDL.TTF
 import Data.Lens.Common
 import Graphics.UI.SDL.Image (load)
 import Reactive.Banana.Frameworks (Frameworks, changes, reactimate)
+import Control.Monad (void)
 --import Debug.Trace
 
 over :: Graphic -> Graphic -> Graphic
@@ -25,11 +26,11 @@ render :: Graphic -> Graphic
 render (Graphic x) = Graphic $ \surface -> x surface >> SDL.flip surface
 
 withinBox :: Rect -> Graphic -> GraphicOpt
-withinBox r g = \r' ->
+withinBox r g r'=
     if r `intersect` r' then emptyG else g
 
 overOpt :: GraphicOpt -> GraphicOpt -> GraphicOpt
-overOpt g1 g2 = \r -> g1 r `over` g2 r
+overOpt g1 g2 r =  g1 r `over` g2 r
 
 overUpdate :: GraphicOpt -> GraphicUpdate -> GraphicUpdate
 overUpdate g1 (g2,r) = (g1 `overOpt` g2, r)
@@ -52,20 +53,20 @@ between :: Int -> (Int,Int) -> Bool
 between x (l,h) = x >= l && x <= h
 
 instance Draw SDL.Surface Mask where
-    draw src mask = Graphic $ \dst -> discard $ blitSurface src clip dst offset
+    draw src mask = Graphic $ \dst -> void $ blitSurface src clip dst offset
         where
             clip = maskClip ^$ mask
-            offset = Just $ Rect { rectX = maskX ^$ mask, rectY = maskY ^$ mask, rectW = 0, rectH = 0 }
+            offset = Just Rect { rectX = maskX ^$ mask, rectY = maskY ^$ mask, rectW = 0, rectH = 0 }
 
 instance Draw Fill Mask where
-    draw fill mask = Graphic $ \dst -> pixel dst >>= \c -> discard $ fillRect dst clip c
+    draw fill mask = Graphic $ \dst -> pixel dst >>= \c -> void $ fillRect dst clip c
         where
             pixel dst = (mapRGB . surfaceGetPixelFormat) dst (colorRed color) (colorGreen color) (colorBlue color)
             clip = fillClip ^$ fill
             color = fillColor ^$ fill
 
 instance Draw Text Mask where
-    draw text mask = Graphic $ \dst -> discard $ blitText dst
+    draw text mask = Graphic $ \dst -> void $ blitText dst
         where
             blitText dst = do
                 --sz <- textSize (textFont ^$ text) (textMsg ^$ text)
@@ -73,10 +74,10 @@ instance Draw Text Mask where
                 blitSurface txt clip dst offset
                 freeSurface txt
             clip = maskClip ^$ mask
-            offset = Just $ Rect { rectX = maskX ^$ mask, rectY = maskY ^$ mask, rectW = 0, rectH = 0 }
+            offset = Just Rect { rectX = maskX ^$ mask, rectY = maskY ^$ mask, rectW = 0, rectH = 0 }
 
 instance Draw Image Mask where
-    draw img mask = Graphic $ \dst -> discard $ blitText dst
+    draw img mask = Graphic $ \dst -> void $ blitText dst
         where
             blitText dst = do
                 --sz <- textSize (textFont ^$ text) (textMsg ^$ text)
@@ -84,32 +85,32 @@ instance Draw Image Mask where
                 blitSurface is clip dst offset
                 freeSurface is
             clip = maskClip ^$ mask
-            offset = Just $ Rect { rectX = maskX ^$ mask, rectY = maskY ^$ mask, rectW = 0, rectH = 0 }
+            offset = Just Rect { rectX = maskX ^$ mask, rectY = maskY ^$ mask, rectW = 0, rectH = 0 }
 
 
 instance Draw AlignedText Rect where
-    draw text rect = Graphic $ \dst -> discard $ blitText dst
+    draw text rect = Graphic $ \dst -> void $ blitText dst
         where
             blitText dst = do
-                (w,h) <- textSize (textFont ^$ atextText ^$ text) (textMsg ^$ atextText ^$ text)
-                let x=case atextHAlign ^$ text of
-                        Start->rectX rect
-                        Middle->rectX rect+(((rectW rect)- w) `div` 2)
-                        End->rectX rect+(rectW rect)-w
-                let y=case atextVAlign ^$ text of
-                        Start->rectY rect
-                        Middle->rectY rect+(((rectY rect)- h) `div` 2)
-                        End->rectY rect+(rectY rect)-h        
-                let offset = Just $ Rect { rectX = x, rectY = y, rectW = 0, rectH = 0 }
+                tr<-getTextRect text rect
+                let offset = Just Rect { rectX = rectX tr, rectY = rectY tr, rectW = 0, rectH = 0 }
                 txt <- renderTextBlended (textFont ^$  atextText ^$ text) (textMsg ^$  atextText ^$ text) (textColor ^$ atextText ^$ text)
                 blitSurface txt Nothing dst offset
                 freeSurface txt
             
-
-
-discard :: IO a -> IO ()
-discard m = m >> return ()
-
+getTextRect :: AlignedText -> Rect -> IO Rect
+getTextRect text rect=do
+  (w,h) <- textSize (textFont ^$ atextText ^$ text) (textMsg ^$ atextText ^$ text)
+  let x=case atextHAlign ^$ text of
+          Start->rectX rect
+          Middle->rectX rect+((rectW rect - w) `div` 2)
+          End->rectX rect + rectW rect-w
+  let y=case atextVAlign ^$ text of
+          Start->rectY rect
+          Middle->rectY rect+((rectY rect - h) `div` 2)
+          End->rectY rect + rectY rect-h     
+  return  Rect { rectX = x, rectY = y, rectW = w, rectH = h }
+   
 renderGraph :: Frameworks t => Behavior t Graphic -> Behavior t Screen -> Moment t ()
 renderGraph bgraph bscreen = do
     egraph <- changes $ render <$> bgraph
